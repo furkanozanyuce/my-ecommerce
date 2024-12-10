fcd67bf920909b3888cd6190ae4501b403be62a8
furkanozanyuce <furkanozanyuce@gmail.com>
2024-12-10 21:53:26 +0300

    used Slider.jsx in HomePage a second time with different data

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,28 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-function Slider() {
-  const slides = [
-    {
-      id: 1,
-      title: "SUMMER 2020",
-      subtitle: "NEW COLLECTION",
-      description:
-        "We know how large objects will act, but things on a small scale.",
-      buttonText: "SHOP NOW",
-      image:
-        "https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FejtigIOws8eu86j53A9M5dXOKwpfNbOYYmYBbwg46T6F~sBE601qt0fKwXD7d0xIjAwPUKrx2~HXMR7Ficu05Hcx4X5aryA1fj9ljSq1s1xIAALuqJj2-E9Q1vyck4tV6v~uKZCM26l3DPRokPuMvQ-mKSxDvkkK9GDv6tvk4fVakHJzCgS~06G0306FElyJsQ79ED5EDdyxiJgCAZ6f7nnWV~zQ~QaB~-xJUGh70jiAh6lSk55CAWOZ~xzmYoT4GsPEYly5U3owlJqk2KkI5pIOGzaKH1w~oDFC0k9Sh40dbL2~fr6Cw9XgbL894Ao4d1lvckyZyajLZoMevulPg__", // Replace with your actual image URL
-    },
-    {
-      id: 2,
-      title: "WINTER SALE",
-      subtitle: "HOT DEALS",
-      description: "Discover our winter collection with amazing discounts.",
-      buttonText: "SHOP NOW",
-      image:
-        "https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FejtigIOws8eu86j53A9M5dXOKwpfNbOYYmYBbwg46T6F~sBE601qt0fKwXD7d0xIjAwPUKrx2~HXMR7Ficu05Hcx4X5aryA1fj9ljSq1s1xIAALuqJj2-E9Q1vyck4tV6v~uKZCM26l3DPRokPuMvQ-mKSxDvkkK9GDv6tvk4fVakHJzCgS~06G0306FElyJsQ79ED5EDdyxiJgCAZ6f7nnWV~zQ~QaB~-xJUGh70jiAh6lSk55CAWOZ~xzmYoT4GsPEYly5U3owlJqk2KkI5pIOGzaKH1w~oDFC0k9Sh40dbL2~fr6Cw9XgbL894Ao4d1lvckyZyajLZoMevulPg__", // Use the same image or different ones if desired
-    },
-  ];
-
+function Slider({ slides }) {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideCount = slides.length;
 
@@ -55,9 +33,10 @@ function Slider() {
               className="w-full flex-shrink-0"
             >
               <div
-                className="relative h-192 bg-center bg-cover"
+                className="relative h-192 bg-[#23856D]"
                 style={{
                   backgroundImage: `url(${slide.image})`,
+                  ...slide.bgStyle,
                 }}
               >
                 <div className="absolute inset-0 flex flex-col items-center md:items-start justify-center md:ml-[100px] text-center px-4 lg:px-12">
@@ -70,9 +49,14 @@ function Slider() {
                   <p className="text-sm sm:text-lg text-white mb-6 px-8 md:px-0">
                     {slide.description}
                   </p>
-                  <button className="px-5 py-2 font-bold bg-green-500 text-white rounded-md shadow-lg hover:bg-green-600 transition">
-                    {slide.buttonText}
-                  </button>
+                  <div className="md:flex gap-3">
+                    <p className="text-2xl sm:text-4xl font-bold text-white">
+                      {slide.price}
+                    </p>
+                    <button className="px-5 py-2 font-bold bg-green-500 text-white rounded-md shadow-lg hover:bg-green-600 transition">
+                      {slide.buttonText}
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -99,11 +83,10 @@ function Slider() {
         {slides.map((_, index) => (
           <button
             key={index}
-            className={`w-3 h-3 rounded-full ${
-              currentSlide === index
+            className={`w-3 h-3 rounded-full ${currentSlide === index
                 ? "bg-white"
                 : "bg-white bg-opacity-50 hover:bg-opacity-75"
-            }`}
+              }`}
             onClick={() => setCurrentSlide(index)}
             aria-label={`Go to slide ${index + 1}`}
           ></button>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,78 @@ import Slider from "../components/Slider";
 import PageContent from "../layout/PageContent";
 
 function HomePage() {
+    const firstSlides = [
+        {
+            id: 1,
+            title: "SUMMER 2020",
+            subtitle: "NEW COLLECTION",
+            description:
+                "We know how large objects will act, but things on a small scale.",
+            buttonText: "SHOP NOW",
+            image:
+                "https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FejtigIOws8eu86j53A9M5dXOKwpfNbOYYmYBbwg46T6F~sBE601qt0fKwXD7d0xIjAwPUKrx2~HXMR7Ficu05Hcx4X5aryA1fj9ljSq1s1xIAALuqJj2-E9Q1vyck4tV6v~uKZCM26l3DPRokPuMvQ-mKSxDvkkK9GDv6tvk4fVakHJzCgS~06G0306FElyJsQ79ED5EDdyxiJgCAZ6f7nnWV~zQ~QaB~-xJUGh70jiAh6lSk55CAWOZ~xzmYoT4GsPEYly5U3owlJqk2KkI5pIOGzaKH1w~oDFC0k9Sh40dbL2~fr6Cw9XgbL894Ao4d1lvckyZyajLZoMevulPg__",
+            bgStyle: {
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            },
+        },
+        {
+            id: 2,
+            title: "WINTER SALE",
+            subtitle: "HOT DEALS",
+            description: "Discover our winter collection with amazing discounts.",
+            buttonText: "SHOP NOW",
+            image:
+                "https://s3-alpha-sig.figma.com/img/96c8/6912/d491d421800e62998b9af7c838cc25d1?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FejtigIOws8eu86j53A9M5dXOKwpfNbOYYmYBbwg46T6F~sBE601qt0fKwXD7d0xIjAwPUKrx2~HXMR7Ficu05Hcx4X5aryA1fj9ljSq1s1xIAALuqJj2-E9Q1vyck4tV6v~uKZCM26l3DPRokPuMvQ-mKSxDvkkK9GDv6tvk4fVakHJzCgS~06G0306FElyJsQ79ED5EDdyxiJgCAZ6f7nnWV~zQ~QaB~-xJUGh70jiAh6lSk55CAWOZ~xzmYoT4GsPEYly5U3owlJqk2KkI5pIOGzaKH1w~oDFC0k9Sh40dbL2~fr6Cw9XgbL894Ao4d1lvckyZyajLZoMevulPg__",
+            bgStyle: {
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            },
+        },
+    ];
+
+    const secondSlides = [
+        {
+            id: 1,
+            title: "SUMMER 2021",
+            subtitle: "Vita Classic Product",
+            description:
+                "We know how large objects will act. We know how are objects will act.",
+            buttonText: "ADD TO CARD",
+            price: "$16.48",
+            image:
+                "https://s3-alpha-sig.figma.com/img/c7a1/9f43/aa4437b65bb40c3e3edb92e61a4d6184?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nEv2bprCj9asXaP7B85Gz6vVs3bjN-S11g7PYry4Veg2QgqSm9wNZoAtrouxsH1~68BS66M1RuePGozS9l97XRfGcRvxyYQvGFpJoh24HCoFtrH-J69h3b0jtpFx6iNWDVMrFD-RwLyTkaJc82aD4XfCZzbxj2HdahMfksnTULxdOwDcuPYNyAZ5RJR~mARrMs6AKcSnRR-TDIaODj7Jz9ZPfnVGCk2pfFkDz7KhEt-qL5177nuvLpXT4T32kb~357htjJ8kYVaTPHhRq9f5i~wBz0j6c8qxpt2NxZdk-wd121hV9Zl8QbttoalPcmLT2x4MIKyYXFUCgzGqBH7n~g__",
+            bgStyle: {
+                backgroundSize: "contain",
+                backgroundPosition: "right",
+                backgroundRepeat: "no-repeat",
+            },
+        },
+        {
+            id: 2,
+            title: "WINTER SALE",
+            subtitle: "Vita Classic Product",
+            description: "Discover our winter collection with amazing discounts.",
+            buttonText: "ADD TO CARD",
+            price: "$16.48",
+            image:
+                "https://s3-alpha-sig.figma.com/img/c7a1/9f43/aa4437b65bb40c3e3edb92e61a4d6184?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nEv2bprCj9asXaP7B85Gz6vVs3bjN-S11g7PYry4Veg2QgqSm9wNZoAtrouxsH1~68BS66M1RuePGozS9l97XRfGcRvxyYQvGFpJoh24HCoFtrH-J69h3b0jtpFx6iNWDVMrFD-RwLyTkaJc82aD4XfCZzbxj2HdahMfksnTULxdOwDcuPYNyAZ5RJR~mARrMs6AKcSnRR-TDIaODj7Jz9ZPfnVGCk2pfFkDz7KhEt-qL5177nuvLpXT4T32kb~357htjJ8kYVaTPHhRq9f5i~wBz0j6c8qxpt2NxZdk-wd121hV9Zl8QbttoalPcmLT2x4MIKyYXFUCgzGqBH7n~g__",
+            bgStyle: {
+                backgroundSize: "contain",
+                backgroundPosition: "right",
+                backgroundRepeat: "no-repeat",
+            },
+        },
+    ];
 
     return (
         <div>
             <PageContent>
-                <Slider />
+                <Slider slides={firstSlides} />
                 <EditorsPick />
+                <Slider slides={secondSlides} />
             </PageContent>
         </div>
     )
