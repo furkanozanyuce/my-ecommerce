6cf0d916d314e29918a4ae0dc6a979ab72941414
furkanozanyuce <furkanozanyuce@gmail.com>
2024-12-10 18:29:44 +0300

    small adjustments to footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -8,54 +8,62 @@ function Footer() {
                 <div>
                     <h3 className="font-bold text-2xl leading-8 tracking-[0.1px] text-[#252B42] ml-[3px]">Bandage</h3>
                 </div>
-                <div className="text-[#23A6F0] flex gap-[20px]">
-                    <Facebook />
-                    <Instagram />
-                    <Twitter />
-                </div>
+                    <ul className="text-[#23A6F0] flex gap-[20px]">
+                        <li className="hover:text-[#252B42]"><a href="#"><Facebook /></a></li>
+                        <li className="hover:text-[#252B42]"><a href="#"><Instagram /></a></li>
+                        <li className="hover:text-[#252B42]"><a href="#"><Twitter /></a></li>
+                    </ul>
             </div>
             <div className="py-[40px] px-[30px] flex flex-col gap-[20px] md:flex-row justify-between">
                 <div className="flex flex-col gap-[15px]">
                     <h5 className="font-bold text-[#252B42] leading-6 text-base tracking-[0.1px]">Company Info</h5>
                     <nav className="list-none font-bold text-[#737373] text-sm leading-6 tracking-[0.2px]">
-                        <li>About Us</li>
-                        <li>Carrier</li>
-                        <li>We are hiring</li>
-                        <li>Blog</li>
+                        <ul>
+                            <li className="hover:text-[#23A6F0]"><a href="#">About Us</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Carrier</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">We are hiring</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Blog</a></li>
+                        </ul>
                     </nav>
                 </div>
                 <div className="flex flex-col gap-[15px]">
                     <h5 className="font-bold text-[#252B42] leading-6 text-base tracking-[0.1px]">Legal</h5>
                     <nav className="list-none font-bold text-[#737373] text-sm leading-6 tracking-[0.2px]">
-                        <li>About Us</li>
-                        <li>Carrier</li>
-                        <li>We are hiring</li>
-                        <li>Blog</li>
+                        <ul>
+                            <li className="hover:text-[#23A6F0]"><a href="#">About Us</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Carrier</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">We are hiring</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Blog</a></li>
+                        </ul>
                     </nav>
                 </div>
                 <div className="flex flex-col gap-[15px]">
                     <h5 className="font-bold text-[#252B42] leading-6 text-base tracking-[0.1px]">Features</h5>
                     <nav className="list-none font-bold text-[#737373] text-sm leading-6 tracking-[0.2px]">
-                        <li>Business Marketing</li>
-                        <li>User Analytic</li>
-                        <li>Live Chat</li>
-                        <li>Unlimited Support</li>
+                        <ul>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Business Marketing</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">User Analytic</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Live Chat</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Unlimited Support</a></li>
+                        </ul>
                     </nav>
                 </div>
                 <div className="flex flex-col gap-[15px]">
                     <h5 className="font-bold text-[#252B42] leading-6 text-base tracking-[0.1px]">Resources</h5>
                     <nav className="list-none font-bold text-[#737373] text-sm leading-6 tracking-[0.2px]">
-                        <li>IOS & Android</li>
-                        <li>Watch a Demo</li>
-                        <li>Customers</li>
-                        <li>API</li>
+                        <ul>
+                            <li className="hover:text-[#23A6F0]"><a href="#">IOS & Android</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Watch a Demo</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">Customers</a></li>
+                            <li className="hover:text-[#23A6F0]"><a href="#">API</a></li>
+                        </ul>
                     </nav>
                 </div>
                 <div className="flex flex-col gap-[15px]">
                     <h5 className="font-bold text-[#252B42] leading-6 text-base tracking-[0.1px]">Get In Touch</h5>
                     <div className="flex h-[58px]">
-                        <input type="text" placeholder="    Your Email" className="border-[1px] border-[#E6E6E6]" />
-                        <button className="bg-[#23A6F0] text-white text-sm font-normal leading-7 w-[120px]">Subscribe</button>
+                        <input type="text" placeholder="Your Email" className="border-[1px] border-[#E6E6E6] rounded-l-md px-4 py-2" />
+                        <button className="bg-[#23A6F0] text-white text-md font-normal leading-7 w-[120px] hover:bg-[#252B42] transition rounded-r-md">Subscribe</button>
                     </div>
                     <p className="font-normal text-xs leading-7	tracking-[0.2px] text-[#737373]">Lore imp sum dolor Amit</p>
                 </div>
