6beb89d4ab2d42f2624c8e2a92966f92ecfabec8
furkanozanyuce <furkanozanyuce@gmail.com>
2024-12-16 06:32:21 +0300

    verifies token and acts according to if it's valid or not

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,47 @@ import SignupForm from './pages/SignupForm'
 import LoginForm from './pages/LoginForm'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import { Provider } from 'react-redux'
-import store from "./redux/store"
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { setUser } from './redux/userActions'
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://workintech-fe-ecommerce.onrender.com',
+});
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+
+    axiosInstance.defaults.headers.common['Authorization'] = token;
+
+    const verifyToken = async () => {
+      try {
+        const response = await axiosInstance.get('/verify');
+        const userData = response.data;
+
+        dispatch(setUser(userData));
+
+        if (userData.token) {
+          localStorage.setItem('token', userData.token);
+          axiosInstance.defaults.headers.common['Authorization'] = userData.token;
+        }
+      } catch (error) {
+        console.error('Token verification failed:', error);
+        localStorage.removeItem('token');
+        delete axiosInstance.defaults.headers.common['Authorization'];
+      }
+    };
+
+    verifyToken();
+  }, [dispatch]);
 
   return (
     <div>
-      <Provider store={store}>
       <Switch>
         <Route path="/" exact>
           <HomePage />
@@ -30,7 +63,6 @@ function App() {
         </Route>
       </Switch>
       <ToastContainer />
-      </Provider>
     </div>
   )
 }
