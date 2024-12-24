881f9b4bfe0c95a47fbaa545e24180dbd4dc0f10
furkanozanyuce <furkanozanyuce@gmail.com>
2024-12-24 06:25:54 +0300

    downloads the imgs for home page instead of linking them

--- src/components/EditorsPick.jsx
+++ src/components/EditorsPick.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const EditorsPick = () => {
   const categories = [
-    { id: 1, title: "MEN", img: "https://s3-alpha-sig.figma.com/img/9da5/ab42/c0357746eb27e42fff6279478e2c8a48?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=q07a0QU3hwnDwBlGs7mpjU02NDpEL9EsayONtNuaGb1ZoQFqXoNy~Fo560tMyRCRdIhdjoNEgYdZpVtd7sFt28vDUa1n1bTBFUAaYgM4K22dXyYQeKF4wFG4iAIleTyJs8ywNNoF4W68Hne9A0oOer-KkVhXV2kHeWBxzmh3zvfyMkn4wL7sbugQ6MorwMWBcaJxyVwRw~MdsPqoegfzerKzyzG04wXOvvdDCZiwOXg3-vIqJoY~SnIDlb4psbtm8jC-XUhKwMN9EGsXNeTyhJqEdi~cOFXuvjYAQQf9VPcprzaw7Y8e~GZEuWRSPrUZXgQuxitaGAnXtogLtX95ow__" },
-    { id: 2, title: "WOMEN", img: "https://s3-alpha-sig.figma.com/img/b384/eba6/08bd8616723a95d25fce7dcb8f25ba9d?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=B5jsthpmJfEKk0b7u0-PoKFBOkG5Y6jMSty7BpIFgG38Q1y9l7iRyIHM7mw9YRVWUnlF8ol2ou9Ea1p657FJzGiTsRbYWHZKqCh4VReKZJL1q7~2AxEBUjUsmOaXlggFHTmaSBx3dPdM3IO-gmsz2OArr~CVTh5sNVQtb7g9J6QuLFVwD~Mn3iMqkD~7yrDRjatUqiYr4ZqxUHSxdh3hgLE4r~WrDSWLReojsg-Ih4nhnv49AWmcmcV680olONcCuQy4jkQd1k7goFeRT5NW~sq2mAnotnZTXNEKPxIuomt1XMTi2KEXWP37L73-0Nvk8tHxpj2rLZtbb6ZI5KTr8g__" },
-    { id: 3, title: "ACCESSORIES", img: "https://s3-alpha-sig.figma.com/img/5077/f785/cbfd4a6cf9efad13d9b5d66b1b7cbf4f?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oyyMcLQcUKfX-csjPLBvzKh~CV0yFPxVq-rXZGCiB5D8JxtsjLV~6Bp9OZMnPI8xFBcwrhDd13CdGmyOIxipmKRCRxHt-r7S~duY5Mrtnz-thrOAoqmZFrg-UkwGc5i7fnMhSX8TPkeIyRMemtixtEKymXa4~FAxAyOXGJ8osTDDm050k2DpY3k~jQn1YxNSwW2TrHHVAUzPYoteb1Z7FQHWKBNFj1BKGm-6z42sfcVQw7OcrUdKd8TR8I1n-iKrvlYG1nGidFwinJjqFTeKKg4Ii~TnWIwfEFBD1yKD9kGFal9jOK0~fqAaQNqaewXRUo18ZnllAhCrcO6WP9ePEw__" },
-    { id: 4, title: "KIDS", img: "https://s3-alpha-sig.figma.com/img/edfc/ad0d/ba1967435dad649ed91c2e00be5f640f?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=eCdJtbPJ96FvyuvIuZh0Pwuxk-xtmCcQ2CH-StGQupaFQ9uNy8mnhahrH-J07QQo7oAUVwdVmML2jhwrvTH8SNNA3w2ztj4cbQ6pT9aLs5zPtD4uuXbOWuZgUqwCkGATixxJAR9oydhSYgAZp6p-QkjD8fBWeQgemyxrV3s2y0razvrwW6a2GlQiaENsvYWbEt~62ZmDL7bK7OUVESQhWEJFSA63e-tsMvmtNpP57WhCyElTuum6xuiP~u92SpncCqBq-gLM9BETOJyzE2xPksdNajKS3EaRqEXA2cfS7IEDN66ZBogxdHmltKlpvykgGqETfdQT49Or7BtE-kWTuw__" },
+    { id: 1, title: "MEN", img: "/images/pick1.jpg" },
+    { id: 2, title: "WOMEN", img: "/images/pick2.jpg" },
+    { id: 3, title: "ACCESSORIES", img: "/images/pick3.jpg" },
+    { id: 4, title: "KIDS", img: "/images/pick4.jpg" },
   ];
 
   return (
